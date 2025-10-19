a64a77995f25fc3e4f9bc9f742352150fa6f977e
shirato_senshi <pangsport666@gmail.com>
2025-10-19 23:10:50 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿const contextMenu = document.getElementById('contextMenu')
+console.log(contextMenu)
+
+function contextHandler(event) {
+  console.log(event)
+
+  event.preventDefault()
+
+  if (contextMenu.style.display === 'none') {
+
+    contextMenu.style.left = event.pageX + 'px'
+    contextMenu.style.top = event.pageY + 'px'
+    contextMenu.style.display = 'block'
+  } else {
+    contextMenu.style.left = event.pageX + 'px'
+    contextMenu.style.top = event.pageY + 'px'
+  }
+
+  console.log(event);
+}
+
+
+function clickContextMenu() {
+  contextMenu.style.display = 'none'
+}
+
+function keyDownHandler(event) {
+  if (event.keyCode === 27)
+    contextMenu.style.display = 'none'
+}
+
+document.body.addEventListener('contextmenu', contextHandler)
+document.body.addEventListener('click', clickContextMenu)
+document.body.addEventListener('keydown', keyDownHandler)
